102c0943a0259fa589c9b8b0ea444e089845e289
Jinho Hyeon <neverlish@gmail.com>
2016-08-14 20:58:09 +0900

    이모션 목록 페이지 정리

--- app/controllers/emotions_controller.rb
+++ app/controllers/emotions_controller.rb
@@ -1,7 +1,7 @@
 class EmotionsController < ApplicationController
 	def emotion
 		@emotion = Emotion.find_by(name: params[:name])
-		@posts = @emotion.posts.paginate(:page => params[:page])
+		@posts = @emotion.posts.order('created_at DESC').paginate(:page => params[:page])
 	end
 	def index
 		@emotions = Emotion.all
